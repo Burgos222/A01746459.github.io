52722a5fada64a40e7eaa69ef1abff59140e5904
A01746459 <A01746459@itesm.mx>
2023-10-03 16:27:07 -0600

    simon

--- app/components/carousel.jsx
+++ app/components/carousel.jsx
@@ -1,5 +1,5 @@
 'use client';
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react'
 
 function ImageCarousel({ images }) {
   const [currentIndex, setCurrentIndex] = useState(0);
