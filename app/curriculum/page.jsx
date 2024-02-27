236dac72820206e7bf93a1bfd1650969b0fe26da
A01746459 <A01746459@itesm.mx>
2024-02-27 13:15:15 -0600

    Cambios de curriculum

--- app/curriculum/page.jsx
+++ app/curriculum/page.jsx
@@ -4,7 +4,7 @@ export default function Proyectos() {
   return (
         <div className="w-[100vw] h-[100vh]">
           <iframe
-            src="../resources/CV_SebastianBurgos.pdf" 
+            src="../resources/Sebastián-Burgos-Alanís-Resume.pdf" 
             title="PDF Viewer"
             width="100%"
             height="100%"
