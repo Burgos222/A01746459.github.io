8f82eaff7481df5a89354bfb4773e647ff98574d
A01746459 <sebasburgos02@gmail.com>
2024-10-23 11:51:09 -0600

    new resume

--- app/curriculum/page.jsx
+++ app/curriculum/page.jsx
@@ -4,7 +4,7 @@ export default function Proyectos() {
   return (
         <div className="w-[100vw] h-[100vh]">
           <iframe
-            src="../resources/Sebastian_Burgos_Resume.pdf" 
+            src="../resources/Sebastián-Burgos-Alanís-Resume-2024.pdf" 
             title="PDF Viewer"
             width="100%"
             height="100%"
